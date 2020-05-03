e998873a68b09b77bd102c4be6cf3245942438fe
Seth J. Connell <sethjordanconnell@gmail.com>
2020-05-03 13:17:06 -0500

    Updated Filter To Write Filtered Results To Files

--- filter.py
+++ filter.py
@@ -1,9 +1,19 @@
+def Clearfile():
+    f = open("results/unusedemails.txt", "w")
+    f.write("")
+    f.close()
+
+def addEmail(x):
+    f = open("results/unusedemails.txt", "a")
+    f.write(str(x))
+    f.close()
+
 def CheckFile(x):
     with open('subsandunsubs.txt', "r") as subsandunsubs:
         if str(x) not in subsandunsubs.read():
-            print(str(x))
+            addEmail(str(x))
             return
-
+Clearfile()
 with open('totalemails.csv') as f:
     lines = f.readlines()
     for email in lines:
